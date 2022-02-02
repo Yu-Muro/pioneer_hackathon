7a10ae98acc3a54609323558372f8dfa89d793ef
Manabu Nishimura <nishimura@db.is.i.nagoya-u.ac.jp>
2022-02-03 04:29:53 +0900

    変数名修正

--- app/src/page/Home/Home.js
+++ app/src/page/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useStopwatch } from "react-timer-hook";
 import axios from 'axios';
 
@@ -12,17 +12,17 @@ const Home = () => {
 
 
   const { seconds, minutes, hours, days, isRunning, start, pause, reset } =
-  useStopwatch({ autoStart: false });
+    useStopwatch({ autoStart: false });
 
 
-  const [start_longitude,setStart_longitude] = useState(); //スタート経度
-  const [start_latitude,setStart_latitude] = useState(); //スタート緯度
+  const [start_longitude, setStart_longitude] = useState(); //スタート経度
+  const [start_latitude, setStart_latitude] = useState(); //スタート緯度
 
-  const [goal_longitude,setGoal_longitude] = useState(); //ゴール経度
-  const [goal_latitude,setGoal_latitude] = useState(); //ゴール緯度
- 
+  const [goal_longitude, setGoal_longitude] = useState(); //ゴール経度
+  const [goal_latitude, setGoal_latitude] = useState(); //ゴール緯度
 
-  const setStartPos = ()=>{
+
+  const setStartPos = () => {
     navigator.geolocation.getCurrentPosition(
       pos => {
         setStart_latitude(pos.coords.latitude);
@@ -31,21 +31,21 @@ const Home = () => {
     )
   };
 
-  const setGoalPos = ()=>{
-      navigator.geolocation.getCurrentPosition(
-        pos => {
-          setStart_latitude(pos.coords.latitude);
-          setStart_longitude(pos.coords.longitude);
-        },
-      )
+  const setGoalPos = () => {
+    navigator.geolocation.getCurrentPosition(
+      pos => {
+        setGoal_latitude(pos.coords.latitude);
+        setGoal_longitude(pos.coords.longitude);
+      },
+    )
   };
 
   //https://developer.yahoo.co.jp/webapi/map/openlocalplatform/v1/distance.html
-  const getDistance = async()=>{
+  const getDistance = async () => {
     await axios.get(`https://map.yahooapis.jp/dist/V1/distance?coordinates=${start_longitude},${start_latitude} ${goal_longitude},${goal_latitude}&appid=ID!!&output=json`)
-    .then(res=>{
-      console.log("###",res)
-    })
+      .then(res => {
+        console.log("###", res)
+      })
 
   }
 
@@ -68,27 +68,27 @@ const Home = () => {
         <button className='mx-1 btn btn-primary' onClick={start}>Start</button>
         <button className='mx-1 btn btn-primary' onClick={pause}>Pause</button>
         <button className='mx-1 btn btn-primary' onClick={reset}>Reset</button>
-          {hours==0 && (
-              <p>安全運転で行きましょう！</p>
-          )}
-          {0<hours  && hours<=1 && (
-              <p>いい調子です!!安全運転</p>
-          )}
-          {1<hours  && hours<=2 && (
-              <p>安全運転えらい！</p>
-          )}
-          {2<hours  && hours<=3 &&(
-              <p>そろそろ休憩したら？</p>
-          )}
-          {3<hours  && hours<=4 &&(
-              <p>3時間！お疲れ様です</p>
-          )}
-          {4<hours  && hours<=5 &&(
-              <p>休憩も取って安全運転しよう！</p>
-          )}
-          {5<hours  && hours<=6 &&(
-              <p>5時間を超えたよ！休憩は取った？</p>
-          )}
+        {hours == 0 && (
+          <p>安全運転で行きましょう！</p>
+        )}
+        {0 < hours && hours <= 1 && (
+          <p>いい調子です!!安全運転</p>
+        )}
+        {1 < hours && hours <= 2 && (
+          <p>安全運転えらい！</p>
+        )}
+        {2 < hours && hours <= 3 && (
+          <p>そろそろ休憩したら？</p>
+        )}
+        {3 < hours && hours <= 4 && (
+          <p>3時間！お疲れ様です</p>
+        )}
+        {4 < hours && hours <= 5 && (
+          <p>休憩も取って安全運転しよう！</p>
+        )}
+        {5 < hours && hours <= 6 && (
+          <p>5時間を超えたよ！休憩は取った？</p>
+        )}
 
 
         <div>
@@ -110,8 +110,8 @@ const Home = () => {
       </p>
 
       <div className='text-center'>
-        <button type='button'className='btn btn-primary' onClick={setStartPos}>start</button>
-        <button type='button'className='btn btn-primary' onClick={setGoalPos}>goal</button>
+        <button type='button' className='btn btn-primary' onClick={setStartPos}>start</button>
+        <button type='button' className='btn btn-primary' onClick={setGoalPos}>goal</button>
         {/* <button type='button'className='btn btn-primary' onClick={getDistance}>d</button> */}
       </div>
 
