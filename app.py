8ee794ae9479389f29a89aed095bb7a3f78e45fe
Yu-Muro <6u38e7c16rnhg5e37a0z@gmail.com>
2022-02-02 16:35:10 +0900

    初期設定

---
+++ app.py
@@ -0,0 +1,17 @@
+import flask
+
+SECRET_KEY = "hogefuga"
+
+app = flask.Flask(
+    __name__,
+    template_folder="templates" # HTMLファイルの置き場所を設定
+)
+app.config["SECRET_KEY"] = SECRET_KEY
+
+@app.route("/", methods = ["POST", "GET"])
+def index():
+    return flask.render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=808080, debug=True)
