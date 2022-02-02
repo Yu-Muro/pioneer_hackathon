5cf1d517dd6e7a7330fa76fb211615c2b0af9851
Yu-Muro <6u38e7c16rnhg5e37a0z@gmail.com>
2022-02-03 04:31:05 +0900

    db周りとAPIをjsonを返すように

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import flask
-import csv
-
+from flask import jsonify
+import db_manager
 
 
 SECRET_KEY = "hogefuga"
@@ -11,11 +11,14 @@ app = flask.Flask(
 )
 app.config["SECRET_KEY"] = SECRET_KEY
 
+
 @app.route("/", methods = ["POST", "GET"])
 def index():
-    return flask.render_template("index.html")
+    flask.abort(404)
+    return None
 
-@app.route("/register", methods = ["POST", "GET"])
+
+@app.route("/register", methods = ["POST"])
 def regisiter():
     if (flask.request.method == "POST"):
         
@@ -24,23 +27,46 @@ def regisiter():
         confirmation = flask.request.form.get('confirmation')
         
         if not username:
-            return flask.redirect('/register')
+            # return flask.redirect('/register')
+            return jsonify({
+                "message": "Account creation failed",
+                "cause": "required user name"
+            }), 400
         elif not password:
-            return flask.redirect('/register')
+            # return flask.redirect('/register')
+            return jsonify({
+                "message": "Account creation failed",
+                "cause": "required password"
+            }), 400
         elif not confirmation:
-            return flask.redirect('/register')
+            # return flask.redirect('/register')
+            return jsonify({
+                "message": "Account creation failed",
+                "cause": "required password"
+            }), 400
         
         if password != confirmation:
-            return flask.redirect('/register')
+            # return flask.redirect('/register')
+            return jsonify({
+                "message": "Account creation failed",
+                "cause": "not matched password"
+            }), 400
         else:
-            ##csv入力 まだ書いてない
-            System_list = [username, password]
-            with open('test.csv', 'a') as file:
-                writer = csv.writer(file, lineterminator='n')
-                writer.writerow(System_list)
-            return flask.redirect('/login')
-    else:
-        return flask.render_template("register.html")
+            # ##csv入力 まだ書いてない
+            # System_list = [username, password]
+            # with open('test.csv', 'a') as file:
+            #     writer = csv.writer(file, lineterminator='n')
+            #     writer.writerow(System_list)
+            if db_manager.get_user(username) != []:
+                return jsonify({
+                    "message": "Account creation failed",
+                    "cause": "already same user name is used"
+                }), 400
+            db_manager.add_new_user(username, password)
+            return jsonify({
+                "message": "Account successfully created"}), 200
+    # else:
+    #     return flask.render_template("register.html")
 
 @app.route("/login", methods = ["POST", "GET"])
 def login():
@@ -53,21 +79,25 @@ def login():
 
         # must provide username
         if not username:
-            return flask.redirect('/login')
+            # return flask.redirect('/login')
+            return jsonify({
+                "message": "Account creation failed",
+                "cause": "required user name"
+            }), 400
 
         # must provide password
         elif not password:
-            return flask.redirect('/login')
+            # return flask.redirect('/login')
+            return jsonify({
+                "message": "Account creation failed",
+                "cause": "required password"
+            }), 400
 
-        #  csvとあってるか まだ書いてない
-
-
-    else:
-        return flask.render_template("login.html")
+        if db_manager.is_exist(username, password):
+            return jsonify({
+                "message": "Account successfully login"}), 200
+    # else:
+    #     return flask.render_template("login.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=808080, debug=True)
-
-    
-
-
