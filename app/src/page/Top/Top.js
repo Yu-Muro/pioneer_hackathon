53e6ed48ffcb8b7ddfe54663e3fcaac429ec6965
Manabu Nishimura <nishimura@db.is.i.nagoya-u.ac.jp>
2022-02-03 13:09:43 +0900

    topを変更

--- app/src/page/Top/Top.js
+++ app/src/page/Top/Top.js
@@ -27,15 +27,22 @@ const Top = () => {
       </div>
 
       <h3 className="text-center">こんなアプリ</h3>
+      <div class="row justify-content-center">
+        <div class="col-10 border-top border-primary border-5"></div>
+      </div>
+      <p></p>
       <div className="row">
         <div className="col-4 text-center border border-2 rounded-lg">
           <h5 className="text-primary">走る</h5>
+          Pixel Driveが走った距離を計測します
         </div>
         <div className="col-4 text-center border border-2 rounded-lg">
-          <h5>描く</h5>
+          <h5 className="text-primary">描く</h5>
+          距離に応じて素敵なドット絵が描かれていきます
         </div>
         <div className="col-4 text-center border border-2 rounded-lg">
-          <h5>集める</h5>
+          <h5 className="text-primary">集める</h5>
+          たくさんのドット絵をゲットしましょう
         </div>
       </div>
     </div>
